9b11f9970e5e901767375ed9012522aa49e70c1b
nice0001-angelo <nice0001@gmail.com>
2019-10-07 20:54:26 +0900

    update

---
+++ excise4.py
@@ -0,0 +1,15 @@
+#excise4.py
+import sys
+n=[]
+n = int(sys.argv[1])
+lost = (sys.argv[2])
+reserve = (sys.argv[3])
+print(n)
+
+def solution(n, lost, reserve):
+    for i
+    n=n
+    answer = 0
+    return answer
+
+
