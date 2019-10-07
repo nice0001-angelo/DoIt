4d505f6f6fc86f79fbddab929201268ec642ffb7
nice0001-angelo <nice0001@gmail.com>
2019-10-07 16:14:35 +0900

    Update excise3.py
    update

--- excise3.py
+++ excise3.py
@@ -6,7 +6,7 @@ def gugu(argv):
     print("%s단입니다\n" % argv)
     for i in range(1,9):
         result = argv*i
-        print(result,end=' ')
+        print("%d * %d = %d\n" % (argv,i,result))
 
 argv = int(sys.argv[1])
 gugu(argv)
