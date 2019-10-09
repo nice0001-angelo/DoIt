c2de997a931382db6b7040f3bf4dea1730e985f2
nice0001-angelo <nice0001@gmail.com>
2019-10-09 16:43:53 +0900

    add

--- excise5.py
+++ excise5.py
@@ -1,51 +1,39 @@
 '''
 def solution(answers):
-    answer = []
-    a = [1,2,3,4,5]
-    b = [2,1,2,3,2,4,2,5]
-    c = [3,3,1,1,2,2,4,4,5,5]
-    _a = a*int(10000/len(a))
-    _b = b*int(10000/len(b))
-    _c = c*int(10000/len(c))
-    ___a = _a[0:len(answers)]
-    print(___a)
-    ___b = _b[0:len(answers)]
-    ___c = _c[0:len(answers)]
-    print(answers)
-    __a = [x for x in ___a if x not in answers]
-    print(__a)
-    __b = [y for y in ___b if y not in answers]
-    __c = [z for z in ___c if z not in answers]
-    if len(__a) > len(__b):
-        if len(__a) > len(__c):
-            answer = [1]
-        elif len(__a) == len(__c):
-            answer = [1,3]
-        else:
-            answer = [3]
-    elif len(__a) == len(__b):
-        if len(__a) == len(__c):
-            answer = [1,2,3]
-        elif len(__a) > len(__c):
-            answer = [1,2]
-    else:
-        answer = [2]
-    return answer
-'''
+    student = [[1, 2, 3, 4, 5],
+               [2, 1, 2, 3, 2, 4, 2, 5],
+               [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    score = [0] * len(student)
 
-
-def solution(answers):
-    p = [[1, 2, 3, 4, 5],
-         [2, 1, 2, 3, 2, 4, 2, 5],
-         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-    s = [0] * len(p)
-
-    for q, a in enumerate(answers):
-        for i, v in enumerate(p):
-            if a == v[q % len(v)]:
-                s[i] += 1
-    return [i + 1 for i, v in enumerate(s) if v == max(s)]
-
+    for answers_k, answers_v in enumerate(answers):
+        for student_k, student_v in enumerate(student):
+            if answers_v == student_v[answers_k % len(student_v)]:
+                score[student_k] += 1
+    return [student_k + 1 for student_k, student_v in enumerate(score) if student_v == max(score)]
 
 if __name__ == "__main__":
-    print(solution([1,2,4,5,1,5,4,2,3]))
+    print(solution([4,5,4,3,2,1]))
+'''
+
+def solution(answers):
+    student1 = [1,2,3,4,5]
+    student2 = [2,1,2,3,2,4,2,5]
+    student3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0,0,0]
+    result = []
+
+    for a_k, a_v in enumerate(answers):
+        if a_v == student1[a_k%len(student1)]:
+            score[0] +=1
+        if a_v == student2[a_k%len(student2)]:
+            score[1] +=1
+        if a_v == student3[a_k%len(student3)]:
+            score[2] +=1
+
+    for s_k, s_v in enumerate(score):
+        if s_v == max(score):
+            result.append(s_k+1)
+    return result
+
+if __name__ == "__main__":
+    print(solution([4,5,4,3,2,1]))
