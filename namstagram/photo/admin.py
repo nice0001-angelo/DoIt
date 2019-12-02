9382152d88c27ee1f8b2f90d95f64a4942189a67
nice0001-angelo <nice0001@gmail.com>
2019-12-02 17:07:14 +0900

    update

--- namstagram/photo/admin.py
+++ namstagram/photo/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Photo
 
-admin.site.register(Photo)
+# 편리하게 하기 위해서 관리자 목록 화면을 바꾸었음
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id','author','created','updated']
+    raw_id_fields = ['author']
+    list_filter = ['created','updated','author']
+    search_fields = ['text','created']
+    ordering = ['-updated','-created']
+
+admin.site.register(Photo, PhotoAdmin)
