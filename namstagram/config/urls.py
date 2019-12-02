642ce667af066b87180e9f9ca6d6c32a26a039de
nice0001-angelo <nice0001@gmail.com>
2019-12-02 17:28:52 +0900

    update

--- namstagram/config/urls.py
+++ namstagram/config/urls.py
@@ -19,3 +19,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
